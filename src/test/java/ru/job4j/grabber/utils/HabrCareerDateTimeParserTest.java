345ac03501013c830249298294ad75b74d09a598
stkuper <stklipper@gmail.com>
2023-11-24 21:17:02 +0300

    2.1. Преобразование даты [#289476]

--- src/test/java/ru/job4j/grabber/utils/HabrCareerDateTimeParserTest.java
+++ src/test/java/ru/job4j/grabber/utils/HabrCareerDateTimeParserTest.java
@@ -13,18 +13,18 @@ class HabrCareerDateTimeParserTest {
     @Test
     public void testDate1() {
         assertThat(dateTimeParser.parse("2023-11-24T20:58:35+03:00"))
-                .isEqualTo(LocalDateTime.of(2023, Month.NOVEMBER, 24, 20, 58,35));
+                .isEqualTo(LocalDateTime.of(2023, Month.NOVEMBER, 24, 20, 58, 35));
     }
 
     @Test
     public void testDate2() {
         assertThat(dateTimeParser.parse("2022-01-07T10:28:55+03:00"))
-                .isEqualTo(LocalDateTime.of(2022, Month.JANUARY, 7, 10, 28,55));
+                .isEqualTo(LocalDateTime.of(2022, Month.JANUARY, 7, 10, 28, 55));
     }
 
     @Test
     public void testDate3() {
         assertThat(dateTimeParser.parse("2024-07-21T22:32:07+03:00"))
-                .isEqualTo(LocalDateTime.of(2024, Month.JULY, 21, 22, 32,7));
+                .isEqualTo(LocalDateTime.of(2024, Month.JULY, 21, 22, 32, 7));
     }
 }
