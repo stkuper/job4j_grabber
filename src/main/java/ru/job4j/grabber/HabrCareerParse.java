797080b7730eb05798fdbf373abd81fe873fa057
stkuper <stklipper@gmail.com>
2023-12-03 13:38:45 +0300

    2.3. Загрузка деталей поста. [#285212 #463910]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -5,6 +5,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.utils.DateTimeParser;
 import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.io.IOException;
@@ -26,7 +27,7 @@ public class HabrCareerParse {
 
     public static void main(String[] args) throws IOException {
         HabrCareerParse habrCareerParse = new HabrCareerParse();
-        HabrCareerDateTimeParser habrCareerDateTimeParser = new HabrCareerDateTimeParser();
+        DateTimeParser timeParser = new HabrCareerDateTimeParser();
         for (int pageNumber = 1; pageNumber <= PAGE_LIMIT; pageNumber++) {
             System.out.printf("Подключаюсь к %d странице%n", pageNumber);
             String fullLink = "%s%s%d%s".formatted(SOURCE_LINK, PREFIX, pageNumber, SUFFIX);
@@ -41,7 +42,7 @@ public class HabrCareerParse {
                 String vacancyName = titleElement.text();
                 String datetime = dateElement.attr("datetime");
                 String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-                LocalDateTime date = habrCareerDateTimeParser.parse(datetime);
+                LocalDateTime date = timeParser.parse(datetime);
                 System.out.printf("%s %s Вакансия выставлена: %3$tFT%3$tT%n",
                         vacancyName, link, date);
                 try {
